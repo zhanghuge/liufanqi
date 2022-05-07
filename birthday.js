15cfa964f66258d507624b58791434fec5ed22fd
张沪歌 <104250325+zhanghuge@users.noreply.github.com>
2022-05-07 19:48:22 +0800

    5-7

--- birthday.js
+++ birthday.js
@@ -1,18 +1,17 @@
-
-      window.onload = function () {
-        var image = document.getElementById("first");
-        var next = document.getElementById("next");
-        var prev = document.getElementById("prev");
-        count = 7;
-        inDex = 1;
-        next.onclick = function () {
-          inDex = inDex + 1 > count ? count : inDex + 1;
-          image.src =
+window.onload = function() {
+    var image = document.getElementById("first");
+    var next = document.getElementById("next");
+    var prev = document.getElementById("prev");
+    count = 7;
+    inDex = 1;
+    next.onclick = function() {
+        inDex = inDex + 1 > count ? count : inDex + 1;
+        image.src =
             "https://zhanghuge.github.io/liufanqi/image/" + inDex + ".jpg";
-        };
-        prev.onclick = function () {
-          inDex = inDex - 1 <= 0 ? count : inDex - 1;
-          image.src =
+    };
+    prev.onclick = function() {
+        inDex = inDex - 1 <= 0 ? count : inDex - 1;
+        image.src =
             " https://zhanghuge.github.io/liufanqi/image/" + inDex + ".jpg";
-        };
-      };
+    };
+};
